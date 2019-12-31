2f4fe289d32ff35323698be750a83bf2d494cc39
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-12-31 18:31:00 -0500

    added documentation

--- R/generator.R
+++ R/generator.R
@@ -1,14 +1,29 @@
+#' Generates an author contribution table
+#'
+#' @param df data frame containing author contribution information
+#'   in two columns: Author, Activity. See example for details.
+#' @import ggplot2
+#' @return ggplot of author contribution table
+#' @examples
+#' # NK participated in Concept and Design
+#' # RW wrote code
+#' # JS participated in Concept and Design
+#' info <- data.frame(Author = c("NK", "NK", "RW", "JS", "JS"),
+#'                    Activity = c("Concept", "Design", "Code", "Concept",
+#'                                 "Design"))
+#' # grey implies author participation in activity; white does not
+#' contribution_table(info)
 contribution_table <- function(df) {
-  df <- expand.grid(x = levels(stuff$Author), y = levels(stuff$Activity))
-  for (i in 1:nrow(df)) {
-    if (nrow(subset(stuff, Author == df$x[i] & Activity == df$y[i])) > 0) {
-      df$Fill[i] <- "grey"
+  df2 <- expand.grid(x = levels(df$Author), y = levels(df$Activity))
+  for (i in 1:nrow(df2)) {
+    if (nrow(subset(df, Author == df2$x[i] & Activity == df2$y[i])) > 0) {
+      df2$Fill[i] <- "grey"
     } else {
-      df$Fill[i] <- "white"
+      df2$Fill[i] <- "white"
     }
   }
 
-  ggplot(df, aes(y, x, fill = I(Fill))) +
+  ggplot(df2, aes(y, x, fill = I(Fill))) +
     geom_tile(colour = I("black")) +
     theme_minimal() +
     theme(panel.grid = element_blank(),
