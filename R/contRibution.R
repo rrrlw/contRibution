94747ded291b56d720a4f887eef92c61c48f0900
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-12-31 18:35:41 -0500

    added package documentation

---
+++ R/contRibution.R
@@ -0,0 +1,10 @@
+#' contRibution: Visual documentation of project contributions
+#'
+#' Implements generation of a table to document author contributions to
+#' research papers and projects as an alternative to paragraphs describing the
+#' same. This replaces wordy descriptions with a visual and intuitive
+#' standard.
+#'
+#' @docType package
+#' @name contRibution
+NULL
